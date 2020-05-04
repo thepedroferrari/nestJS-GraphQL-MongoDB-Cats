ec540571eb0c33696d3fe935457b8430ac31cb9b
Pedro Ferrari <pedro@pedroferrari.com>
2020-05-04 11:06:38 +0200

    Working Draft adding mutations

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost/nest')
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
